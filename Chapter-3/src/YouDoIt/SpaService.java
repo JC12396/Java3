021b4525f17158db5e97736cd2887f1820bb83f4
Joseph Chalupa <grammerking113@gmail.com>
2018-09-14 09:46:29 -0500

    Debug and YouDoIT

--- Chapter-3/src/YouDoIt/SpaService.java
+++ Chapter-3/src/YouDoIt/SpaService.java
@@ -2,49 +2,29 @@ package YouDoIt;
 
 import java.util.Scanner;
 
-public class SpaService {
-	private String serviceDescription;
-	private double price;
-	
-	public static void main(String[] args) 
-	{
-		String service;
-		double price;
-		
-		SpaService firstService = new SpaService();
-		SpaService secondService = new SpaService();
-		Scanner keyboard = new Scanner(System.in);
-		
-		System.out.print("Enter service >> ");
-		service = keyboard.nextLine();
-		System.out.print("Enter price >> ");
-		price = keyboard.nextDouble();
-		
-	}
-	
-	public void setServiceDescription(String service)
-	{
-		serviceDescription = service;
-	}
-	
-	public void setPrice(double pr)
-	{
-		price = pr;
-	}
-	
-	public String getServiceDescription()
-	{
-		return serviceDescription;
-	}
-	
-	public double getPrice()
-	{
-		return price;
-	}
-	
-	public class CreateSpaServices
-	{
-
-	}
-
+public class SpaService
+{
+   private String serviceDescription;
+   private double price;
+   public SpaService()
+   {
+      serviceDescription = "XXX";
+      price = 0;
+   }
+   public void setServiceDescription(String service)
+   {
+      serviceDescription = service;
+   }
+   public void setPrice(double pr)
+   {
+      price = pr;
+   }
+   public String getServiceDescription()
+   {
+      return serviceDescription;
+   }
+   public double getPrice()
+   {
+      return price;
+   }
 }
